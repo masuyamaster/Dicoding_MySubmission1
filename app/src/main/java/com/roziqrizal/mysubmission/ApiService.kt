28a70937cd6f8c99dc72696be8f07b06a8f0050f
Roziq Rizal <86276259+masuyamaster@users.noreply.github.com>
2022-03-15 14:30:01 +0700

    add new token

--- app/src/main/java/com/roziqrizal/mysubmission/ApiService.kt
+++ app/src/main/java/com/roziqrizal/mysubmission/ApiService.kt
@@ -6,25 +6,25 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getGithubUsers(
         @Query("q") username: String
     ): Call<ResponseSearch>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getGithubUsersDetail(
         @Path("username") username: String
     ): Call<ResponseGithubUsersDetail>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun  getUsersDetailFollower(
         @Path("username") username: String
     ): Call<ArrayList<ResponseFollowerItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun  getUsersDetailFollowing(
         @Path("username") username: String
     ): Call<ArrayList<ResponseFollowerItem>>
